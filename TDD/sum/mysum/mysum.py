223acf2675b54a486753aa232b09957fdafd5ca2
misoi <cytnhia.chepkemoi@gmail.com>
2016-08-18 23:02:32 +0300

    a file containing a mysum class

---
+++ TDD/sum/mysum/mysum.py
@@ -0,0 +1,18 @@
+# class MySum(object):
+#     def __init__(self, y, x):
+#         self.y = y
+#         self.x = x
+def my_sum(x, y):
+    '''
+    takes two numbers and return (x and y) and returns its sum
+    '''
+    if type(x) or type(y) == str:
+        return "Invalid input"
+    elif ty(x) or type(y) == list:
+    	return "invalid input"
+    else:
+        return x + y
+    
+
+    
+
